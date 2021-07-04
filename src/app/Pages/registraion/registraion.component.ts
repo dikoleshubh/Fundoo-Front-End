7c61960ce3279b9754be8e7c1f4f0ccf8507ea8a
dikoleshubh <dikole.shubh@gmail.com>
2021-07-05 00:10:35 +0530

    Refractor to Code - Password Hide/Show

--- src/app/Pages/registraion/registraion.component.ts
+++ src/app/Pages/registraion/registraion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { Router } from '@angular/router';
+import {FormBuilder, FormGroup, FormControl, Validators,} from '@angular/forms';
 import { UserserviceService } from 'src/app/Services/user/user.service';
 
 @Component({
@@ -11,25 +12,43 @@ import { UserserviceService } from 'src/app/Services/user/user.service';
 
 export class RegistraionComponent implements OnInit {
 
-    constructor(private service: UserserviceService) { }
+
+    constructor(private service: UserserviceService) {
+     
+     }
   
     form = new FormGroup({
       firstName: new FormControl('', [Validators.required, Validators.minLength(1)]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(1)]),
       email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(1)]),
       password: new FormControl('', [Validators.required, Validators.minLength(1)])
+      
     })
   
-    hide : Boolean = false
+    hide : Boolean = true
+    
   
     ngOnInit(): void {
     }
+    
+    TogglePassword(){
+      this.hide = this.hide ? false : true 
+    }
+    
   
     get f() {
       return this.form.controls;
     }
   
-    
+    passType: string = 'password';
+
+    toggleshow(){
+    if(this.passType== 'password'){
+    this.passType= 'text'
+    }else{
+    this.passType== 'password'
+    }
+    }
   
     submit() {
       console.log(this.form.valid);
